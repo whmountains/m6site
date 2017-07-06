5232cf37eb48368439a2144980e5f56c110b79c9
Daniel Atwood <danielatwood@Daniels-MacBook-Pro.local>
2017-07-06 19:39:58 -0400

    Added PhotoSwipe Gonna need to do a little `yarn install` to make sure you get PhotoSwipe Also I don't think its working 100% right now. /testing to check it out

---
+++ src/components/MNav.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+import { lighten } from 'polished'
+
+export const HEIGHT = '60px'
+
+const BACKGROUND = `#2C5E86`
+const ACCENT = `rgba(25,118,210,1)`
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 99;
+
+  background: ${BACKGROUND};
+  border-bottom: 5px solid ${ACCENT};
+  box-sizing: border-box;
+  height: ${HEIGHT};
+
+  display: flex;
+  padding: 0 15px;
+`
+
+const Logo = styled.img`
+
+`
+
+const Name = styled.h1`
+  color: #fff;
+  align-self: center;
+  margin: 0;
+  margin-right: auto;
+  font-size: 35px;
+  font-weight: 500;
+`
+
+const Links = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+`
+
+const NavLink = styled(Link)`
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ padding: 0 15px;
+ color: white;
+ font-size: 22px;
+ text-decoration: none;
+ cursor: pointer;
+
+ &.active {
+   background: #1976d2;
+ }
+
+ *:hover > & {
+   background: transparent;
+
+   &:hover {
+     background: #1976d2;
+   }
+ }
+`
+
+export default () => {
+
+  return (
+    <Container>
+      <Logo /> {/* TODO: fill in this image */}
+      <Name>El Refugio</Name>
+      <Links>
+        <NavLink activeClassName='active' to='/'>Home</NavLink>
+        <NavLink activeClassName='active' to='/lodge/'>Accomodations</NavLink>
+        <NavLink activeClassName='active' to='/lake/'>Activities</NavLink>
+        <NavLink activeClassName='active' to='/gallery/'>Gallery</NavLink>
+        <NavLink activeClassName='active' to='/booking/'>Book Now</NavLink>
+      </Links>
+    </Container>
+  )
+}
---
+++ src/pages/testing.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {PhotoSwipeGallery} from 'react-photoswipe';
+
+let items = [
+  {
+    src: 'http://lorempixel.com/1200/900/sports/1',
+    thumbnail: 'http://lorempixel.com/120/90/sports/1',
+    w: 1200,
+    h: 900,
+    title: 'Image 1'
+  },
+  {
+    src: 'http://lorempixel.com/1200/900/sports/1',
+    thumbnail: 'http://lorempixel.com/120/90/sports/1',
+    w: 1200,
+    h: 900,
+    title: 'Image 1'
+  },
+  {
+    src: 'http://lorempixel.com/1200/900/sports/1',
+    thumbnail: 'http://lorempixel.com/120/90/sports/1',
+    w: 1200,
+    h: 900,
+    title: 'Image 1'
+  },
+  {
+    src: 'http://lorempixel.com/1200/900/sports/2',
+    thumbnail: 'http://lorempixel.com/120/90/sports/2',
+    w: 1200,
+    h: 900,
+    title: 'Image 2'
+  }
+]
+
+let options = { // http://photoswipe.com/documentation/options.html
+}
+
+const getThumbnailContent = (item) => { // The const was added and was not in the original code from npm
+  return (
+    <img src={item.thumbnail} width={120} height={90} />
+  )
+}
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <PhotoSwipeGallery items={items} options={options} thumbnailContent={getThumbnailContent} />
+      </div>
+    )
+  }
+}
