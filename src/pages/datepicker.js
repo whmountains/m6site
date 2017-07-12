8b4b7df93dfcaf5d4d6290e056203f18507caa65
Caleb Whiting <caleb@whiting.io>
2017-07-12 14:10:49 -0400

    fix the datepicker

--- src/pages/datepicker.js
+++ src/pages/datepicker.js
@@ -1,19 +1,22 @@
-// import React from 'react'
-// import styled from 'styled-components'
-//
-// import Calendar from '../components/DatePicker'
-//
-// const Blue = {
-//   background: 'blue',
-//   height: '90vh'
-// }
-//
-// export default class Index extends React.Component {
-//   render () {
-//     return (
-//       <div style={Blue}>
-//         <Calendar />
-//       </div>
-//     )
-//   }
-// }
+import React from 'react'
+import styled from 'styled-components'
+
+import Calendar from '../components/DatePicker'
+
+console.log('datepicker', Calendar)
+
+const Blue = {
+  background: 'blue',
+  height: '90vh'
+}
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div style={Blue}>
+        <Calendar />
+        test
+      </div>
+    )
+  }
+}
